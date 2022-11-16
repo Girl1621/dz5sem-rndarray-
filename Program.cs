058a401572f41e6b29c444724b73c45188d539e3
Ksenia <Kseniakuzevanova@gmail.com>
2022-11-16 19:09:56 +0700

    finish

--- Program.cs
+++ Program.cs
@@ -1,42 +1,42 @@
 ﻿//Задайте массив заполненный случайными положительными трёхзначными числами.
 //Напишите программу, которая покажет количество чётных чисел в массиве
 
-// int[] array = new int[4];
-// FillArray(array);
-// PrintArray(array);
-// int result = FindEvenNumbers(array);
-// Console.Write($"Количество четных чисел в массиве: {result} ");
-// // выводим массив
-// void PrintArray(int[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write($"{array[i]} ");
-//     }
-// }
-// //заполняем массив
-// void FillArray(int[] array)
-// {
-//     Random rnd = new Random();
-//     for (var i = 0; i < array.Length; i++)
-//     {
-//         array[i] = rnd.Next(100, 999);
-//     }
-// }
-// //находим четные числа
-// int FindEvenNumbers(int[] array)
-// {
-//     int count = 0;
+int[] array = new int[4];
+FillArray(array);
+PrintArray(array);
+int result = FindEvenNumbers(array);
+Console.Write($"Количество четных чисел в массиве: {result} ");
+// выводим массив
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+//заполняем массив
+void FillArray(int[] array)
+{
+    Random rnd = new Random();
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 999);
+    }
+}
+//находим четные числа
+int FindEvenNumbers(int[] array)
+{
+    int count = 0;
 
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (array[i] % 2 == 0) 
-//         {
-//             count++;
-//         }
-//     }
-//     return count;
-// }
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
 // Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
@@ -82,64 +82,65 @@
 // Задайте массив вещественных чисел. Найдите разницу между 
 // максимальным и минимальным элементов массива.
 
-int[] arr = new int[5];
-FillArray(arr);
-PrintArray(arr);
-int result1 = FindMaxValue(arr);
-int result2 = FindMinValue(arr);
-Console.Write($"Больший элемент массива = {result1} ");
-Console.Write($"Меньший элемент массива = {result2} ");
+// int[] arr = new int[5];
+// FillArray(arr);
+// PrintArray(arr);
+// int result1 = FindMaxValue(arr);
+// int result2 = FindMinValue(arr);
+// Console.Write($"Больший элемент массива = {result1} ");
+// Console.Write($"Меньший элемент массива = {result2} ");
+// int difference = result1 - result2;
+// Console.Write($"разница между максимальным и минимальным элементом массива = {difference} ");
+// //выводим массив
+// void PrintArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.Write($"{arr[i]} ");
+//     }
+// }
 
-//выводим массив
-void PrintArray(int[] arr)
-{
-    for (int i = 0; i < arr.Length; i++)
-    {
-        Console.Write($"{arr[i]} ");
-    }
-}
+// //заполняем массив
+// void FillArray(int[] arr)
+// {
+//     Random rnd = new Random();
+//     for (var i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = rnd.Next(0, 101);
+//     }
+// }
 
-//заполняем массив
-void FillArray(int[] arr)
-{
-    Random rnd = new Random();
-    for (var i = 0; i < arr.Length; i++)
-    {
-        arr[i] = rnd.Next(0, 101);
-    }
-}
+// int FindMinValue(int[] arr)
+// {
+//     int minValue = arr[0];
+//     for (int i = 1; i < arr.Length; i++)
+//     {
+//         if (arr[i] < minValue)
+//         {
+//             minValue = arr[i];
 
-int FindMinValue(int[] arr)
-{
-    int minValue = arr[0];
-    for (int i = 1; i < arr.Length; i++)
-    {
-        if (arr[i] < minValue)
-        {
-            minValue = arr[i];
+//         }
 
-        }
+//     }
+//     return minValue;
 
-    }
-    return minValue;
+// }
 
-}
+// int FindMaxValue(int[] arr)
+// {
+//     int maxValue = arr[0];
+//     for (int i = 1; i < arr.Length; i++)
+//     {
+//         if (arr[i] > maxValue)
+//         {
+//             maxValue = arr[i];
 
-int FindMaxValue(int[] arr)
-{
-    int maxValue = arr[0];
-    for (int i = 1; i < arr.Length; i++)
-    {
-        if (arr[i] > maxValue)
-        {
-            maxValue = arr[i];
+//         }
 
-        }
+//     }
+//     return maxValue;
 
-    }
-    return maxValue;
-
-}
+// }
 
 
 
